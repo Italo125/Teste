8a70a088086ea426e45d29c87d9eddd30bb40a12
Italo Gomes <43956763+Italo125@users.noreply.github.com>
2023-07-23 17:41:51 -0300

    commit Teste Técnico - Software Engineer Backend

---
+++ Teste-Matriz/TesteMatri/TesteMatri/Matriz.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TesteMatri;
+
+
+internal class Matriz
+{
+    public int linhas, colunas;
+
+    public string[,] matriz;
+
+    public bool MatrizQuadrada = false;
+
+
+
+
+    public Matriz(int linhas, int colunas)
+    {
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.matriz = new string[this.linhas, this.colunas];
+
+        this.gerarMatriz();
+
+
+        if (this.linhas == this.colunas)
+        {
+           this.MatrizQuadrada = true;
+
+        }
+
+    }
+
+    public void gerarMatriz()
+    {
+        for (int i = 0; i < this.linhas; i++)
+        {
+            for (int j = 0; j < this.colunas; j++)
+            {
+
+                matriz[i, j] = Console.ReadLine(); //(int) new Random().Next() * 100_000 / 100;
+            }
+        }
+    }
+
+    public void printMatriz()
+    {
+        for (int i = 0; i < this.linhas; i++)
+        {
+            for (int j = 0; j < this.colunas; j++)
+            {
+
+                Console.Write(matriz[i, j] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+
+    public string diagonalPrincipal()
+    {
+        string[] vet = new string[this.colunas];
+        string[] Princ = new string[this.colunas];
+        // guardar diagonal primária da matriz
+        for (int i = 0; i < this.linhas; i++)
+        {
+            for (int j = 0; j < this.colunas; j++)
+            {
+                if (i == j)
+                {
+                    vet[i] = matriz[j, i];
+                    Princ[i] = vet[i];
+                    Console.Write(" ", vet[i]);
+                }
+            }
+        }
+
+        return Princ[this.colunas];
+    }
+
+    public string diagonalSecundaria()
+    {
+        string[] vet2 = new string[this.colunas];
+        string[] Secun = new string[this.colunas];
+
+        Console.WriteLine("Guardando diagonal secundária da  matriz");
+
+        // guardar diagonal secundária da  matriz
+        for (int i = 0; i < this.linhas; i++)
+        {
+            for (int j = 0; j < this.colunas; j++)
+            {
+                if (i + j == this.colunas - 1)
+                {
+                    vet2[i] = matriz[i, j];
+                    Secun[i] = vet2[i];
+                    Console.Write(" ", vet2[i]);
+                }
+            }
+
+        }
+
+        return Secun[this.colunas];
+
+    }
+
+    public void trocaDiagonal()
+    {
+        for (int i = 0; i < this.linhas; i++)
+        {
+            for (int j = 0; j < this.colunas; j++)
+            {
+                if (i == j)     // diagonal primaria
+                {
+                    matriz[i, j] = diagonalSecundaria();
+                    {
+
+                    }
+                    if (i + j == 3) // diagonal secundaria
+                    {
+                        matriz[i, j] = diagonalPrincipal();
+                    }
+                }
+            }
+        }
+    }
+
+
+ 
+
+
+
+    //List<List<Integer>> result = new ArrayList<>();
+    //for (int i = rowStart; i <= rowEnd; i++)
+    //{
+    //    List<Integer> row = new ArrayList<>();
+    //    for (int j = colStart; j <= colEnd; j++)
+    //    {
+    //        row.add(matrix[i][j]);
+    //    }
+    //    result.add(row);
+    //}
+
+    //System.out.println("The maximum sum submatrix is " + result);
+    //return maxSum;
+}
+
+
+
+
+
+
---
+++ Teste-Matriz/TesteMatri/TesteMatri/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Security.Cryptography;
+using TesteMatri;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+        //Matriz matriz = new Matriz(, );
+
+
+        //matriz.gerarMatriz();
+
+        //matriz.printMatriz();
+
+        //matriz.diagonalPrincipal();
+        //matriz.diagonalSecundaria();
+
+        //matriz.trocaDiagonal();
+
+        //matriz.printMatriz();
+
+
+
+        // Alterenativa 2 do teste
+
+        int[][] jaggedArray2 = new int[][]
+        {
+            new int[] { 1, 3, 5, 7, 9 },
+            new int[] { 0, 2, 4, 6 ,8},
+            new int[] { 11, 22 ,10, 15,3}
+         };
+
+        int[][] jaggedArray3 = new int[][]
+      {
+            new int[] { 1, 3, 9 },
+            new int[] { 0, 2, 6 },
+            new int[] { 11, 22,10 }
+       };
+
+
+
+
+
+
+
+        Console.WriteLine(  matrizFind(jaggedArray2, jaggedArray3));
+         
+
+
+
+        static int matrizFind(int[][] matrizMaior, int[][] matrizMenor)
+        {
+
+
+            // Computa o tamanho das matrizes. 
+            int alturaMenor = matrizMenor.Length;
+            int larguraMenor = alturaMenor == 0 ? 0 : matrizMenor[0].Length;
+            int alturaMaior = matrizMaior.Length;
+            int larguraMaior = alturaMaior == 0 ? 0 : matrizMaior[0].Length;
+
+            int quantidade = 0;
+
+            // [Opcional] Rejeita matrizes que tiverem linhas com larguras heterogêneas.
+            for (int t = 1; t < alturaMaior; t++)
+            {
+                if (matrizMaior[t].Length != larguraMaior)
+                {
+                    Console.WriteLine("Ambas as matrizes devem ter larguras homogêneas.");
+                }
+            }
+            for (int t = 1; t < alturaMenor; t++)
+            {
+                if (matrizMenor[t].Length != larguraMenor)
+                {
+
+                    Console.WriteLine("Ambas as matrizes devem ter larguras homogêneas.");
+
+                }
+            }
+
+            // Percorre as linhas da matriz maior 
+            for (int a = 0; a <= alturaMaior - alturaMenor; a++)
+            {
+                // Percorre as colunas da matriz maior 
+                for (int b = 0; b <= larguraMaior - larguraMenor; b++)
+                {
+
+                    // Tendo a posição [a][b] da matriz maior como correspondente ao possível canto superior esquerdo da matriz menor,
+                    // verifica se essa posição contém a matriz menor ao percorrer ambas as matrizes juntas 
+                    // Começa percorrendo as linhas de ambas as matrizes.
+                    for (int c = 0; c < alturaMenor; c++)
+                    {
+                        // Percorre as colunas de ambas as matrizes.
+                        for (int d = 0; d < larguraMenor; d++)
+                        {
+
+                            // Se as coordenadas tiverem valores diferentes, então essa posição da matriz maior não contém a menor.
+                            // Dessa forma, se for esse o caso, interrompe o processo de percorrer ambas as matrizes juntas e avança para a
+                            // próxima possibilidade na matriz maior.
+                            if (matrizMaior[a + c][b + d] != matrizMenor[c][d])
+                            {
+                                continue;
+
+
+                            }
+                            else
+                            {
+                                quantidade++;
+                            }
+
+                        }
+                    }
+
+                   
+
+                }
+            }
+
+           
+            return quantidade++;
+        }
+
+
+    }
+}
